95a2393c635dfad17a9c2e3ec1b808f93b6f3e32
youcantknowmeboy <youcantknowme@yandex.ru>
2020-09-22 14:24:54 +0300

    Zadacha4

---
+++ hw_1_4/app.js
@@ -0,0 +1,21 @@
+/* Дано три числа a, b, c - вывести наименьшее из них */
+
+let a = 1
+let b = 2
+let c = 3
+
+if (a < b) {
+  if (a < c) {
+    alert (a)
+  }
+}
+if (b < a) {
+  if (b < c){
+    alert (b)
+  }
+}
+if (c < b){
+  if (c < a) {
+    alert (c)
+  }
+}
